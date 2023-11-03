8d0a58febde8e2f350bee4f9d29d56470b08b7cb
Carlos Cintero <ccinterod02@gmail.com>
2023-11-03 23:32:55 +0000

    comenzamos a codificar

---
+++ src/Console.java
@@ -0,0 +1,7 @@
+
+public class Console {
+
+    public  void println() {
+    }
+
+}
--- src/Game.java
+++ src/Game.java
@@ -1,11 +1,31 @@
 
 public class Game {
 
+    private WhitePlayer whitePlayer;
+    private BlackPlayer blackPlayer;
+    private ChessBoard chessBoard;
+
+    public Game() {
+        whitePlayer = new WhitePlayer();
+        blackPlayer = new BlackPlayer();
+        chessBoard = new ChessBoard();
+    }
+
+
     public boolean isResumed() {
-        return false;
+        String answer;
+        Console console = new Console();
+
+        do {
+            answer = console.readString("¿Do you want to play the rematch? (y/n): ");
+        } while (!answer.equals("y") && !answer.equals("Y") && !answer.equals("n") && !answer.equals("N"));
+        return answer.equals("y") || answer.equals("Y");
     }
 
     public void play() {
+        do {
+            // refactorizar los player para que se use el polimorfismo
+        } while (!whitePlayer.hasWon() && !blackPlayer.hasWon());
     }
 
 }
